2b3923ecfe1f8c780ee57b95613a233639457659
Volkan Sevim <volkansevim@gmail.com>
2014-12-22 16:43:00 -0800

    Update doc, add sample reaction files

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #g++ -Wall -ggdb3 -I/usr/local/include/sundials/  -c  $1.cpp 
 mpicxx -g -O3 -g -Wall -O3 -I/usr/local/include/sundials/  -c  $1.cpp 
 #g++ -L/usr/local/lib -static -pthread $1.o libsundials_cvode.a libsundials_nvecserial.a -lboost_regex -lgsl -lgslcblas -lm 
-mpicxx -g -O3 -g -Wall -O3 -Wl,-Bsymbolic-functions -Wall -L/usr/local/lib $1.o libsundials_cvode.a libsundials_nvecserial.a -lboost_regex -lgsl -lgslcblas
+#mpicxx -g -O3 -g -Wall -O3 -Wl,-Bsymbolic-functions -Wall -L/usr/local/lib $1.o libsundials_cvode.a libsundials_nvecserial.a -lboost_regex -lgsl -lgslcblas
+mpicxx -g -O3 -g -Wall -O3 -Wl,-Bsymbolic-functions -Wall -L/usr/lib $1.o -lsundials_cvode -lsundials_nvecserial -lm -lboost_regex -lgsl -lgslcblas
 ## I removed -static -pthread
 rm $1.o
+
--- compile_test.sh
+++
@@ -1,7 +0,0 @@
-#g++ -Wall -ggdb3 -I/usr/local/include/sundials/  -c  $1.cpp 
-mpicxx -g -O3 -g -Wall -O3 -I/usr/local/include/sundials/  -c  $1.cpp 
-#g++ -L/usr/local/lib -static -pthread $1.o libsundials_cvode.a libsundials_nvecserial.a -lboost_regex -lgsl -lgslcblas -lm 
-###!!! mpicxx -g -O3 -g -Wall -O3 -Wl,-Bsymbolic-functions -Wall -L/usr/local/lib $1.o -lsundials_cvode -lsundials_nvecserial -lboost_regex 
-mpicxx -g -O3 -g -Wall -O3 -Wl,-Bsymbolic-functions -Wall $1.o -lsundials_cvode -lsundials_nvecserial -lboost_regex -lgsl -llapack  
-## I removed -static -pthread
-rm $1.o
